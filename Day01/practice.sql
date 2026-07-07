80b1157e51bdee7b095ab1bba380f786abea8487
Ishankrchaudhary <Ishan.kr.chaudhary@gmail.com>
2026-07-07 20:52:48 +0545

    Create practice.sql for SQL basics overview
    Added SQL basics practice file covering topics, skills, and reflections.

---
+++ Day01/practice.sql
@@ -0,0 +1,31 @@
+# Day 1 – SQL Basics
+
+## Topics Covered
+
+* Introduction to SQL
+* Creating a database
+* Creating tables
+* Modifying tables using `ALTER TABLE`
+* Adding new columns
+* Modifying existing columns
+* Renaming tables and columns
+* Deleting columns
+* Basic SQL syntax and commands
+
+## What I Learned
+
+Today, I learned the fundamentals of SQL and how to work with database tables. I practiced creating tables, modifying their structure using the `ALTER TABLE` command, adding and removing columns, and making changes to existing tables. I also became familiar with basic SQL syntax and how to organize data within a database.
+
+## Skills Gained
+
+* Create a new database
+* Create tables
+* Add columns to a table
+* Modify table structure
+* Rename tables and columns
+* Delete columns
+* Understand basic SQL commands
+
+## Reflection
+
+Today was a great introduction to SQL. I now understand how to create and manage the structure of a database. I'm looking forward to learning how to insert, retrieve, update, and delete data in the next lessons.
